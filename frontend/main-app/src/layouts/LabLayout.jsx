e62e8002d8ae041bfcc00bc8cb1fb62abf0a825f
Vijayendra Singh <vizz.bob@gmail.com>
2026-07-09 16:09:07 +0530

    Fix remaining lab_Pages -> lab_pages casing in LabLayout.jsx

--- frontend/main-app/src/layouts/LabLayout.jsx
+++ frontend/main-app/src/layouts/LabLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import LabSidebar from "../modules/clinicadmin/pages/lab_Pages/LabSidebar";
+import LabSidebar from "../modules/clinicadmin/pages/lab_pages/LabSidebar";
 
 export default function LabLayout() {
     return (
